042bbc893802599d13869ff55e10f1943be88f45
BetoSandoval <alberto.sa1391@gmail.com>
2023-10-15 14:15:25 -0600

    Se elimina importacion no utilizada

--- src/components/VisualComponentForm/VisualComponentForm.test.jsx
+++ src/components/VisualComponentForm/VisualComponentForm.test.jsx
@@ -1,8 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import VisualComponentForm from "./VisualComponentForm";
-import { describe, test , expect, jest} from '@jest/globals';
+import { describe, test , expect } from '@jest/globals';
 
 describe('Testing on <VisualComponentForm/>', () => {
   test('should disable the button when input is not a natural number', async () => { 
